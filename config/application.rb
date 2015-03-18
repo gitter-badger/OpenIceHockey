635dfca0d92ab9087bdd60ce12c8879bcbc0e57a
Phil Hughes <theephil@gmail.com>
2015-03-18 20:56:12 +0000

    Updated config files for Rails 4.2

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,10 @@ module Livehockey
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    # API config vars
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
     config.api_url = "http://livehockey.dev"
     config.api_version = "v1"
   end
