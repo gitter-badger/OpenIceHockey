313cce413a0585d45f26956180a1290caf672a88
Phil Hughes <theephil@gmail.com>
2015-03-18 11:39:58 +0000

    Fixed tests

--- test/controllers/api/v1/teams_controller_test.rb
+++ test/controllers/api/v1/teams_controller_test.rb
@@ -10,12 +10,12 @@ class TeamsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should return data for all teams" do
+  test "should return items for all teams" do
     get :all
     assert_response :success
 
     jsonHash = JSON.parse(response.body)
-    assert_equal jsonHash["data"].length, 2
+    assert_equal jsonHash["items"].length, 2
   end
 
   test "staff member endpoint url returned" do
@@ -23,7 +23,7 @@ class TeamsControllerTest < ActionController::TestCase
     assert_response :success
 
     jsonHash = JSON.parse(response.body)
-    team = jsonHash["data"][0]
+    team = jsonHash["items"][0]
     assert_equal team["staff_members_url"], "http://livehockey.dev/api/v1/team/1/staff"
   end
 
@@ -37,6 +37,6 @@ class TeamsControllerTest < ActionController::TestCase
     assert_response :success
 
     jsonHash = JSON.parse(response.body)
-    assert_equal jsonHash["data"].length, 1
+    assert_equal jsonHash["items"].length, 1
   end
 end
